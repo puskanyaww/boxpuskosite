111074e42a5cc2ee194255cd38c7a52188679800
puskanyaww <solmi15.art@gmail.com>
2024-02-22 17:40:46 +0300

    new

---
+++ script.js
@@ -0,0 +1,40 @@
+const socket = io("ws://trgu.ru:4020", {transports: ['websocket', 'polling', 'flashsocket']});
+
+let id;
+
+function connect(){
+    const name = document.getElementById("name").value
+    const code = document.getElementById("code").value.toUpperCase()
+    socket.emit('clientLogin', {name: name, code: code});
+    socket.on('youJoined', (data) => {
+        id = data.id
+        console.log(data.name)
+        document.getElementById("content").innerHTML = `
+        <p>Ваше имя ${data.name}<p/>
+        <p id="task">Ожидание начала игры</p>
+        `
+    })
+    socket.on('errorJoin', (data) => {
+        alert(data)
+    })
+}
+
+socket.on('task', (data) => {
+    console.log(id)
+    if(data.taskName == "chaosText"){
+        document.getElementById("content").innerHTML = `
+    <h3 id="task">${data.title}</h3>
+    <input type="text" placeholder="Писать тут" id="taskplace">
+    <button onclick="sendTask()">Отправить</button>
+    `
+    }
+})
+
+
+socket.on('gameState', (data) => {
+
+    if(data.state == "logo"){
+        document.getElementById("content").innerHTML = `
+    `
+    }
+})
